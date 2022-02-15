7ab1b9ce4fe2fe8a536d4465f54e5f5f5c013dc7
William Ledo <williamledoc@gmail.com>
2022-02-14 21:24:58 -0300

    complete project

---
+++ src/main/java/com/williamledo/firstproject/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.williamledo.firstproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.williamledo.firstproject.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+}
---
+++ src/main/java/com/williamledo/firstproject/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.williamledo.firstproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.williamledo.firstproject.entities.Product;
+
+@Repository	
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
